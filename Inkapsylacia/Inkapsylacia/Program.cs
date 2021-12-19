e463b23320a2071b80ea2a9eafaf13f7ea17a98d
Galinka <galinka2002niko@gmail.com>
2021-12-19 12:42:17 +0300

    DS

---
+++ Inkapsylacia/Inkapsylacia/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Inkapsylacia
+{
+
+    class Gun
+    {
+        private bool isLoaded; //состояние
+
+        private void Reload() //опознает, скрытая реализация
+        {
+            Console.WriteLine("Заряжаю...");
+
+            isLoaded = true;
+
+            Console.WriteLine("Заряжено!");
+        }
+
+        public void Shoot()// управляет выстрелом
+
+        {
+            if (!isLoaded)
+            {
+                Console.WriteLine("Орудие не заряжено!");
+                Reload();
+
+            }
+            Console.WriteLine("Выстрел\n");
+            isLoaded = false;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Gun gun = new Gun();
+            gun.Shoot();
+        }
+    }
+}
